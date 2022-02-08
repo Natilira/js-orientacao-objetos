2b727a5b28c3f9098cdad1e00b7b15e7a281a3cf
Natalia Lira <ncl@cesar.org.br>
2022-02-08 11:57:35 -0300

    curso2 aula3

--- Conta.js
+++ Conta.js
@@ -1,51 +1,53 @@
-export class Conta{
-    constructor(saldoInicial, cliente, agencia){
+//Classe abstrata
+export class Conta {
+    constructor(saldoInicial, cliente, agencia) {
+        if (this.constructor == Conta) {
+            throw new Error("você não deveria instanciar um objeto do tipo Conta diretamente, pois essa é uma classe abstrata");
+        }
+
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
     }
 
-    set cliente(novoValor){
-        if(novoValor instanceof Cliente){
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
             this._cliente = novoValor;
         }
     }
 
-    get cliente(){
+    get cliente() {
         return this._cliente;
     }
-    
-    get saldo(){
+
+    get saldo() {
         return this._saldo;
     }
-
+    
+    // Método abstrato
     sacar(valor) {
-        let taxa = 1;
-        this._sacar(valor, taxa);
-        return this._sacar(valor, taxa)
+        throw new Error("O método sacr da conta é abstrato");
     }
 
-    _sacar(valor, taxa){
+    _sacar(valor, taxa) {
         const valorSacado = taxa * valor;
-        if (this._saldo >= valorSacado){
+        if (this._saldo >= valorSacado) {
             this._saldo -= valorSacado;
             return valorSacado;
-        } 
-        
+        }
+
         return 0;
     }
 
     depositar(valor) {
-        if(valor <= 0){ 
-            return; 
-        }                
+        if (valor <= 0) {
+            return;
+        }
         this._saldo += valor;
     }
 
     transferir(valor, conta) {
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-       
     }
-
 }
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import { Cliente } from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 const cliente1 = new Cliente('Ricardo',11422233309);
 
 
 const contaCorrenteRicardo = new ContaCorrente (cliente1 , 1001);
-contaCorrenteRicardo.depositar (500);
-contaCorrenteRicardo.sacar (100);
-
 const contapoupanca = new ContaPoupanca (50 , cliente1, 1001);
-contapoupanca.sacar(10);
+//const conta = new Conta(0, cliente1, 1001);
+const contaSalario = new ContaSalario (cliente1);
 
-console.log(contapoupanca);
-console.log(contaCorrenteRicardo);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
+
+console.log(contaSalario);
+//console.log(contaCorrenteRicardo);
