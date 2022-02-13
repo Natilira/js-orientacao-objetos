e51990e721dd18c90cb0e57033f84dbc95bc018d
Natalia Lira <ncl@cesar.org.br>
2022-02-13 13:41:38 -0300

    curso2 aula5

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -1,10 +1,19 @@
 /*
 ser autenticavel significa ter o metodo autenticar 
+
+ducky type, se ele anda como um, faz quaqua como um , deve ser um pato. não interessa o tipo do objeto
+me interesssa oq ele pode fazer se ele tem a presença de determinados metódos e propriedades.
 */
 
 export class SistemaAutenticacao{
     static login(autenticavel, senha){
+        if (SistemaAutenticacao.ehAutenticavel(autenticavel)){
         return autenticavel.autenticar(senha);
-
+        }
+        return false;
+    }
+    // ele será um metódo estatico, que não será necessario uma instancia para chamar ele  
+    static ehAutenticavel(autenticavel){
+        return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function 
     }
 }
